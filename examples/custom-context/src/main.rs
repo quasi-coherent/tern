cb7227d55caeb09c4e5fa67964016e124bc6b3fe
Daniel Donohue <d.michael.donohue@gmail.com>
2025-01-08 21:50:27 -0500

    unused dep

--- examples/custom-context/src/main.rs
+++ examples/custom-context/src/main.rs
@@ -3,7 +3,6 @@ use tern::App;
 
 #[tokio::main]
 async fn main() {
-    dotenvy::dotenv().ok();
     let mut env = env_logger::Builder::from_default_env();
     env.filter(None, log::LevelFilter::Info).init();
 
